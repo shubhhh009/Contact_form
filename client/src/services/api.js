0a02ba5d7e38d9b61d3e33613d6c7d5e919701d9
Shubham Tiwari <tiwarisachin8482@gmail.com>
2026-01-09 11:18:09 +0530

    Ready for deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "/api",
+    baseURL: "https://contact-form.vercel.app/api",
 })
 
 export const createContact = (data) => API.post("/contacts", data);
